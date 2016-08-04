0a9f6698e4fbf4fff45faf6221747c758ba67a55
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-08-04 11:25:40 -0400

    Added in the rest of my art to the seed file and got art#index thumbnails running

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,55 @@ Comment.destroy_all
 
 current_art = Art.create!({
   title: "God of Wine",
-  file_path: "/app/assets/art_work/god_of_wine.jpg"})
+  file_path: "/assets/god_of_wine.jpg"})
+
+other_art = Art.create!([
+  {title: "Cannibal Kid",
+  file_path: "/assets/cannibal.jpg"},
+  {title: "Just Wanted to Say Hello",
+  file_path: "/assets/hello.jpg"},
+  {title: "Home Mirror",
+  file_path: "/assets/home_mirror.jpg"},
+  {title: "King Mirror",
+   file_path: "/assets/king_mirror.jpg"},
+   {title: "Liberia Mantra",
+   file_path: "/assets/lib_mantra.jpg"},
+  {title: "Pink Tutus and Automatica Weapons",
+   file_path: "/assets/pink_tutus.jpg"},
+  {title: "PolyMathic Beauty",
+   file_path: "/assets/poly.jpg"},
+  {title: "MarshMellow Queen",
+   file_path: "/assets/queen.jpg"},
+  {title: "Russian King",
+   file_path: "/assets/russian.jpg"},
+  {title: "Said The Shotgun to the Head",
+   file_path: "/assets/said.jpg"},
+  {title: "O.G. One",
+   file_path: "/assets/og_one.jpg"},
+  {title: "O.G. Two",
+   file_path: "/assets/og_two.jpg"},
+  {title: "O.G. Three",
+   file_path: "/assets/og_three.jpg"},
+  {title: "O.G. Four",
+   file_path: "/assets/og_four.jpg"},
+  {title: "O.G. Five",
+   file_path: "/assets/og_five.jpg"},
+  {title: "#Selfie",
+   file_path: "/assets/selfie.jpg"},
+  {title: "Little Girl In Blue Dress",
+   file_path: "/assets/little_girl.jpg"},
+  {title: "I Like You for Your Narcissistic Personality",
+   file_path: "/assets/narc.jpg"},
+  {title: "No Faith",
+   file_path: "/assets/no_faith.jpg"},
+  {title: "Pablo's Bed",
+   file_path: "/assets/pablo.jpg"}
+
+
+])
+
+
+
 
 current_comment = Comment.create!({
   message: "Beautiful!",
